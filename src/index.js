9524bea63f529feea8562ec7c8cbcf6bd36919b9
Aaron Sandow <aaron.sandow@auth0.com>
2020-12-30 15:50:50 -0500

    Changed App to uppercase, to match what is imported to GitHub and have proper naming conventions.  Hopefully Heroku push works now...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./app.js";
+import App from "./App.js";
 import { BrowserRouter as Router} from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
